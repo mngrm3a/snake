b55be06cef153ed12676e37d062642cf190a5f51
mngrm3a <9266859+mngrm3a@users.noreply.github.com>
2024-06-11 05:30:23 +0200

    Add Main dummy implementation

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,32 @@
 module Main (main) where
 
+import Data.Function ((&))
+import Data.Functor ((<&>))
+import qualified Graphics.Gloss as Gloss
+import Graphics.Gloss.Interface.Environment (getScreenSize)
+
 main :: IO ()
-main = pure ()
+main = do
+  windowPosition <- getCenteredWindowPosition windowSize
+  Gloss.play
+    (Gloss.InWindow windowName windowSize windowPosition)
+    windowColor
+    windowFPS
+    -- initial world
+    ()
+    -- render world
+    (const Gloss.Blank)
+    -- handle events
+    (const id)
+    -- update world
+    (const id)
+  where
+    windowFPS = 30
+    windowName = "Snake"
+    windowSize = (800, 600)
+    windowColor = Gloss.white
+
+getCenteredWindowPosition :: (Int, Int) -> IO (Int, Int)
+getCenteredWindowPosition (winWidth, winHeight) =
+  getScreenSize <&> \(scrWidth, scrHeight) ->
+    (scrWidth - winWidth & flip div 2, scrHeight - winHeight & flip div 2)
