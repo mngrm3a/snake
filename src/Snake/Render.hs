a2b00e60e4b544ca48ffd7035d5bb35010b08ff4
mngrm3a <9266859+mngrm3a@users.noreply.github.com>
2024-06-11 19:21:33 +0200

    Render interpolated segments in Playing state

--- src/Snake/Render.hs
+++ src/Snake/Render.hs
@@ -3,7 +3,10 @@ module Snake.Render (renderWorld) where
 import qualified Graphics.Gloss as Gloss
 import Lens.Micro.Platform ((&))
 import Snake.Render.Background (renderGrid)
-import Snake.Render.Segments (renderSegmentEndPositions)
+import Snake.Render.Segments
+  ( renderInterpolatedSegmentPositions,
+    renderSegmentEndPositions,
+  )
 import Snake.Render.Utils (renderScreenOverlay)
 import Snake.World (State (Collision, GetReady, Playing), World (_state))
 
@@ -26,7 +29,7 @@ renderGetReadyState w =
     overlayColor = Gloss.greyN 0.8 & Gloss.withAlpha 0.65
 
 renderPlayingState :: World -> Gloss.Picture
-renderPlayingState = const Gloss.blank
+renderPlayingState = renderInterpolatedSegmentPositions
 
 renderCollisionState :: World -> Gloss.Picture
 renderCollisionState = const Gloss.blank
--- src/Snake/Render/Segments.hs
+++ src/Snake/Render/Segments.hs
@@ -1,9 +1,30 @@
-module Snake.Render.Segments (renderSegmentEndPositions) where
+module Snake.Render.Segments
+  ( renderSegmentEndPositions,
+    renderInterpolatedSegmentPositions,
+  )
+where
 
 import qualified Graphics.Gloss as Gloss
-import Lens.Micro.Platform
+import Lens.Micro.Platform ((&), (<&>))
 import Snake.Geometry
 import Snake.World
+  ( Segment (Segment, _end),
+    Segments,
+    World (_clock, _segmentSize, _segments),
+    progress,
+    toList,
+  )
+
+renderInterpolatedSegmentPositions :: World -> Gloss.Picture
+renderInterpolatedSegmentPositions w =
+  interpolatedPositions clockProgress segs
+    <&> renderBodySegment bodyColor segSize
+    & Gloss.pictures
+  where
+    clockProgress = w & _clock & progress
+    segs = w & _segments
+    bodyColor = Gloss.azure & Gloss.bright
+    segSize = w & _segmentSize
 
 renderSegmentEndPositions :: World -> Gloss.Picture
 renderSegmentEndPositions w =
@@ -23,3 +44,15 @@ renderBodySegment bodyColor segSize segPos =
 
 endPositions :: Segments -> [PointF]
 endPositions = fmap _end . toList
+
+interpolatedPositions :: Float -> Segments -> [PointF]
+interpolatedPositions t = fmap go . toList
+  where
+    go (Segment s e) = sinusoidalInterpolate t s e
+
+sinusoidalInterpolate :: Float -> PointF -> PointF -> PointF
+sinusoidalInterpolate t = linearInterpolate $ 0.5 * (1 - cos (t * pi))
+
+linearInterpolate :: Float -> PointF -> PointF -> PointF
+linearInterpolate t (V2 x1 y1) (V2 x2 y2) =
+  V2 ((1 - t) * x1 + t * x2) ((1 - t) * y1 + t * y2)
